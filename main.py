eb917e16433cbf70a611ea509f00f07f67d0f2ee
Andrew <60705784+AndrewGordienko@users.noreply.github.com>
2023-07-17 23:15:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ import time
 from random import randint
 from env5 import Simulation
 
-class Agent():
-    def choose_action(self, observation, direction):
-        observation += [direction] * 17
-        return randint(-10, 10)
 
 def simulate_environment(seed):
-    agent = Agent()
-    direction = randint(0, 1)
+    direction_moving = randint(0, 1)
     simulation = Simulation()
-    simulation.run_through(agent, direction)
+    simulation.run_through(direction_moving)
     return simulation.score
 
 if __name__ == '__main__':
-    num_envs = 4000  # Number of environments to run in parallel
+    num_envs = 1  # Number of environments to run in parallel
     pool = multiprocessing.Pool(processes=num_envs)
     
     start_time = time.time()  # Start timer
@@ -24,4 +19,4 @@ if __name__ == '__main__':
     end_time = time.time()  # End timer
     
     print("Number of Enviornments {} Total execution time: {} seconds".format(num_envs, end_time - start_time))
-    print(scores)
+    print(sum(scores)/num_envs)
