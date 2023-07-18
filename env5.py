08e08b2a0c0ecf9c51ed256034f93ef4f384cc32
Andrew <60705784+AndrewGordienko@users.noreply.github.com>
2023-07-17 23:14:55 -0400

    Update env5.py

--- env5.py
+++ env5.py
@@ -3,7 +3,10 @@ import Box2D.b2
 import math
 from random import randint, uniform, shuffle
 from itertools import combinations
+from brain import Agent
+import torch
 
+agent = Agent()
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 # pygame.init()
@@ -90,7 +93,7 @@ class RayCastCallback(Box2D.b2RayCastCallback):
         return 0  # Returning 0 terminates the raycast after the first hit
 
 class Simulation():
-    def run_through(self, agent, direction):
+    def run_through(self, direction_moving):
         creature = Creature(world)
         ground_body = Floor(world)
 
@@ -151,9 +154,11 @@ class Simulation():
             joint_angles = [joint.joint.angle for joint in creature.joints]
             self.observation = raycast_result + touch_inputs + joint_angles
 
+            speeds = agent.choose_action(self.observation, direction_moving)
+            joint_index = 0
             for joint in creature.joints:
-                self.action = randint(-10, 10)
-                joint.joint.motorSpeed = agent.choose_action(self.observation, direction) * 20**3
+                joint.joint.motorSpeed = speeds[joint_index].item()
+                joint_index += 1
 
             # Calculate the distance from the reference position (middle of the floor)
             current_position = creature.shapes[2].box.position
@@ -178,6 +183,52 @@ class Simulation():
                 self.score += -1
             else:
                 self.reward = 0  # Set reward to 0 if there is no change in distance
+            
+            index = 0
+            touch_inputs = [0] * 5
+            raycast_result = [-1.0] * 8  # List to store the distances until contact for the eight directions
+            directions = [0, 1, 2, 3, 4, 5, 6, 7]  # Directions in multiples of 45 degrees
+
+            for shape in creature.shapes:
+                index += 1
+                for fixture in shape.box.fixtures:
+                    sub_shape = fixture.shape
+                    vertices = [shape.box.transform * v for v in sub_shape.vertices]
+
+                    for contact_edge in shape.box.contacts:
+                        contact = contact_edge.contact
+                        if contact.fixtureA.body == shape.box and contact.fixtureB.body == ground_body.ground_body:
+                            touch_inputs[index-1] = 1
+                            break
+
+                    if index == 3:  # For the middle shape (shape number three)
+                        center = shape.box.transform * Box2D.b2Vec2(0, 0)  # Center point of the shape
+                        raycast_length = 100  # Length of the rays
+
+                        callback = RayCastCallback()  # Create an instance of the callback object
+
+                        for direction in directions:
+                            angle = direction * (math.pi / 4)  # Convert the direction to radians
+                            raycast_end = center + raycast_length * Box2D.b2Vec2(math.cos(angle), math.sin(angle))
+
+                            world.RayCast(callback, center, raycast_end)  # Perform the raycast for each direction
+                            if callback.hit:
+                                raycast_result[direction] = raycast_length * callback.distance
+
+                            callback.hit = False  # Reset the hit flag for the next raycast
+
+            joint_angles = [joint.joint.angle for joint in creature.joints]
+            self.observation_ = raycast_result + touch_inputs + joint_angles
+
+            self.observation = list(self.observation)
+            self.observation += [direction_moving] * 17
+            self.observation = torch.Tensor(self.observation)
+
+            self.observation_ = list(self.observation_)
+            self.observation_ += [direction_moving] * 17
+            self.observation_ = torch.Tensor(self.observation_)
+
+            agent.memory.add(self.observation, speeds.detach().numpy(), self.reward, self.observation_, self.done)
 
             # Update the previous distance
             self.prev_distance = distance_from_reference
